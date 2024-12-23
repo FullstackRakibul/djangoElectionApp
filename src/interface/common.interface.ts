cbd67f48594bb1af61fcc4e7b34fc286c5a40bcb
FullstackRakibul <rakibul.cse.47@gmail.com>
2024-12-23 23:52:49 +0600

    address card ready

--- src/interface/common.interface.ts
+++ src/interface/common.interface.ts
@@ -21,18 +21,21 @@ export interface districtListInterface {
 
 
 export interface wordListInterface {
+  id?: number
   ward_name: string;
   ward_name_ban?: string;
   union: number;
 }
 
 export interface unionListInterface {
+  id?: number
   union_name: string;
   union_name_ban?: string;
   upazila: number;
 }
 
 export interface upzillahListInterface {
+  id?: number
   upazila_name: string;
   upazila_name_ban: string;
   district: number;
--- src/views/core/election/electionCenter.vue
+++ src/views/core/election/electionCenter.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-import type { addressInterface, districtListInterface, divisionListInterface } from '@/interface/common.interface';
+import type { addressInterface, districtListInterface, divisionListInterface, unionListInterface, upzillahListInterface, wordListInterface } from '@/interface/common.interface';
 import type { electionCenterInterface } from '@/interface/election.interface';
-import { getDistrictDetailsService, getDistrictListService, getDivisionListService } from '@/services/common.services';
+import { getDistrictDetailsService, getDistrictListService, getDivisionListService, getUnionListService, getUpzillahListService, getWordListService } from '@/services/common.services';
 import { electionCenterCreateService, electionCenterListDataService } from '@/services/election/election-center.services';
 import { message } from 'ant-design-vue';
 import { computed, onMounted, reactive, ref } from 'vue'
@@ -44,7 +44,10 @@ const electionCenterListfetch = async () => {
 onMounted(() => {
   electionCenterListfetch()
   getDivisionList()
-   getDistrictList()
+  getDistrictList()
+  getUpzillahList()
+  getUnionList()
+  getWordList()
 })
 
 const electionCenterDataColumns = [
@@ -84,6 +87,9 @@ const onFinish = async (values: electionCenterInterface) => {
 
 const divisionList = ref(<divisionListInterface[]>[]);
 const districtList = ref(<districtListInterface[]>[]);
+const upazilaList = ref(<upzillahListInterface[]>[]);
+const unionList = ref(<unionListInterface[]>[]);
+const wordList = ref(<wordListInterface[]>[]);
 
 const getDivisionList = async () => {
   try {
@@ -98,6 +104,8 @@ const getDivisionList = async () => {
     console.log("Something went wrong on divison !!!")
   }
 }
+
+// district 
 
 const getDistrictList = async () => {
   try {
@@ -121,10 +129,87 @@ const filteredDistrictList = computed(() =>
   )
 );
 
+// upzillah 
+
+
+const getUpzillahList = async () => {
+  try {
+    
+    const response = await getUpzillahListService()
+    console.log(response)
+    if (response.status === 200) {
+      upazilaList.value = response.data.data
+     
+    } else {
+      message.error("Division response error !!!")
+    }
+
+  } catch (error) {
+    console.log("Something went wrong on upzillah !!!")
+  }
+}
+
+const filteredUpzillatList = computed(() =>
+  upazilaList.value.filter(
+    (upz) => upz.district === addressCreateFrom.district_id
+  )
+);
+
+// union 
+
+const getUnionList = async () => {
+  try {
+    
+    const response = await getUnionListService()
+    console.log(response)
+    if (response.status === 200) {
+      unionList.value = response.data.data
+     
+    } else {
+      message.error("Division response error !!!")
+    }
+
+  } catch (error) {
+    console.log("Something went wrong on upzillah !!!")
+  }
+}
+
+const filteredUniontList = computed(() =>
+  unionList.value.filter(
+    (union) => union.upazila === addressCreateFrom.upazila_id
+  )
+);
+
+// ward
+
+const getWordList = async () => {
+  try {
+    
+    const response = await getWordListService()
+    console.log(response)
+    if (response.status === 200) {
+      wordList.value = response.data.data
+     
+    } else {
+      message.error("Division response error !!!")
+    }
+
+  } catch (error) {
+    console.log("Something went wrong on upzillah !!!")
+  }
+}
+
+const filteredWordList = computed(() =>
+  wordList.value.filter(
+    (word) => word.union === addressCreateFrom.union_id
+  )
+);
+
+
 </script>
 
 <template>
-  <a-row class="flec flex-row justify-center">
+  <a-row class="flec flex-row justify-center items-center">
     <a-col :span="18">
       <a-row :gutter="16" class="flex flex-row justify-between p-3">
         <a-col class="gutter-row" :span="12">
@@ -152,25 +237,46 @@ const filteredDistrictList = computed(() =>
           </a-card>
         </a-col>
 
-
+ 
 
 
         <a-col class="gutter-row" :span="12">
           <a-card title="ঠিকানা খুজে না পাওয়া গেলে ,এখানে তৈরী করুন...">
             <a-form :model="addressCreateFrom" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }" labelAlign="left">
-              <a-form-item label="Select Division" name="division_id">
-                <!-- <a-input v-mode:value="addressCreateFrom.division_id"></a-input> -->
+              
+              <a-form-item label="বিভাগ নির্বাচন করুন" name="division_id">
                 <a-select v-model:value="addressCreateFrom.division_id" showSearch :options="divisionList.map(division => ({
-                  label: division.division_name, value: division.id
+                  label: division.division_name_ban, value: division.id
                 }))">
 
                 </a-select>
               </a-form-item>
-              <a-form-item label="Select District" name="district_id">
+              <a-form-item label="জেলা নির্বাচন করুন" name="district_id">
                 <a-select v-model:value="addressCreateFrom.district_id" showSearch :options="filteredDistrictList.map(district => ({
-                  label: district.district_name, value: district.id
+                  label: district.district_name_ban, value: district.id
                 }))">
                 </a-select>
+              </a-form-item>
+              <a-form-item label="উপজেলা নির্বাচন করুন" name="upazila_id">
+                <a-select v-model:value="addressCreateFrom.upazila_id" showSearch :options="filteredUpzillatList.map(upz => ({
+                  label: upz.upazila_name_ban, value: upz.id
+                }))">
+                </a-select>
+              </a-form-item>
+              <a-form-item label="ইউনিয়ন নির্বাচন করুন" name="upazila_id">
+                <a-select v-model:value="addressCreateFrom.union_id" showSearch :options="filteredUniontList.map(union => ({
+                  label: union.union_name_ban, value: union.id
+                }))">
+                </a-select>
+              </a-form-item>
+              <a-form-item label="ওয়ার্ড নির্বাচন করুন" name="ward_id">
+                <a-select v-model:value="addressCreateFrom.ward_id" showSearch :options="filteredWordList.map(ward => ({
+                  label: ward.ward_name_ban, value: ward.id
+                }))">
+                </a-select>
+              </a-form-item>
+              <a-form-item label="হাউজ নাম্বার / গ্রাম / রাস্তা " name="line1">
+                <a-input  v-model:value="addressCreateFrom.line1" ></a-input>
               </a-form-item>
               <a-form-item>
                 <a-button type="primary" html-type="submit"> {{ isEditing ? 'Update' : 'Save' }}</a-button>
