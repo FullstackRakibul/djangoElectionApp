6962e89b6b9db282f3613045463aa151cf49a5f0
FullstackRakibul <rakibul.cse.47@gmail.com>
2025-01-30 00:47:11 +0600

    fix

--- src/views/core/election/ElectionDetails.vue
+++ src/views/core/election/ElectionDetails.vue
@@ -44,7 +44,7 @@ const electionDetailsColumn = [
     dataIndex:"election_type"
   },
   {
-    title:"Candidates",
+    title:"Candidates", 
     dataIndex:"candidates"
   },
   {
@@ -60,7 +60,7 @@ const electionDetailsColumn = [
 </script>
 
 <template>
-    <a-card title="Election Data" :headStyle="{ borderBottom: '2px solid #1890ff' }">
+    <a-card title="নির্বাচনের ধরন ও বিবরণ" :headStyle="{ borderBottom: '2px solid #1890ff' }">
     <a-table :dataSource="electionDetails" :columns="electionDetailsColumn" :loading="loading">
     </a-table>
   </a-card>
